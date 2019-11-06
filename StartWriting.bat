9cc8365d362f2f6c8217f9bea38878790fb33db7
lanlan2017 <18251956727@163.com>
2019-11-06 16:26:01 +0800

    更新StartWriting.bat,更新Next配置

--- StartWriting.bat
+++ StartWriting.bat
@@ -7,7 +7,7 @@ echo %~dp0
 set blogRoot=%~dp0
 cd %blogRoot%
 REM 拉取远程分支
-git pull origin master
+REM git pull origin master
 REM 在文章根目录下打开文件资源管理器
 set postRoot=%~dp0source\_posts
 explorer %postRoot%
